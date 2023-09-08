6e456ea2eb277c45897a67b6d7e81b7317d5630e
Maha Abdelnabi <mahaomar@gmail.com>
2023-09-08 09:34:03 -0400

    fetch api data

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 btnEl = document.getElementById("btn");
+const animeContainerEl = document.querySelector(".anime-container");
+const animeImgEl = document.querySelector(".anime-img");
+const amineNameEl = document.querySelector(".anime-name");
 
 
-function getAnime(){
-    console.log('clicked')
+async function getAnime(){
+    try {
+        const response = await fetch("https://catboys.com/api/img");
+        const data =await  response.json();
+        console.log(data)
+    } catch (error) {
+        
+    }
 }
 //add eventlistener to btn
 btnEl.addEventListener('click', getAnime);
